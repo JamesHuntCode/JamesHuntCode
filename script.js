5005dff4e5ec0e8ae019d023009ba0ab10d2860d
James Hunt <jameshuntwork1997@yahoo.com>
2018-01-15 20:13:11 +0000

    functional slideshow
    no actual slides yet, slides are represented by colours.

--- script.js
+++ script.js
@@ -4,12 +4,57 @@ $(document).ready(function() {
         $(this).animate({color: '#800080'}, 200);
     }, function() {
         $(this).animate({color: '#ffffff'}, 200);
-    })
+    });
     // END OF NAVIGATION BAR
 
     // SUMMARY (SLIDESHOW)
 
-    
+    // Different slides in slideshow
+    var slideshow = function(topSlide, middleSlide, bottomSlide) {
+        // Method to show the top slide
+        var showTopSlide = function() {
+            topSlide.show();
+            middleSlide.show();
+            bottomSlide.show();
+        }
 
+        // Method to show the middle slide
+        var showMiddleSlide = function() {
+            topSlide.hide();
+            middleSlide.show();
+            bottomSlide.show();
+        }
+
+        // Method to show the bottom slide
+        var showBottomSlide = function() {
+            topSlide.hide();
+            middleSlide.hide();
+            bottomSlide.show();
+        }
+
+        var counter = 1;
+        var slideDuration = 5000;
+
+        // Method to auto-change slides
+        var changeSlides = function() {
+            if (counter === 1) {
+                showTopSlide();
+                counter += 1;
+            } else if (counter === 2) {
+                showMiddleSlide();
+                counter += 1;
+            } else if (counter == 3) {
+                showBottomSlide();
+                counter = 1;
+            }
+        }
+        setInterval(changeSlides, slideDuration);
+    }
+
+    var minigameSlide = $('#minigames');
+    var codingSlide = $('#coding-passion');
+    var websiteSlide = $('#stunning-websites');
+
+    slideshow(websiteSlide, codingSlide, minigameSlide);
     // END OF SUMMARY (SLIDESHOW)
 });
