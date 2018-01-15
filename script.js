bf5bcc0a4bdd56a4fc25e83269e2680ac56e5f4c
James Hunt <jameshuntwork1997@yahoo.com>
2018-01-15 18:56:25 +0000

    navigation bar

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    
+    // NAVIGATION BAR
+    $('#nav-bar a').hover(function() {
+        $(this).animate({color: '#808080'}, 200);
+    }, function() {
+        $(this).animate({color: '#ffffff'}, 200);
+    })
+    // END OF NAVIGATION BAR
 });
