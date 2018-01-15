ce8b6848c5d46d2fc8cb73004061d3f42c6f1d50
James Hunt <jameshuntwork1997@yahoo.com>
2018-01-15 19:11:06 +0000

    getting started on the slideshow

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     // NAVIGATION BAR
     $('#nav-bar a').hover(function() {
-        $(this).animate({color: '#808080'}, 200);
+        $(this).animate({color: '#800080'}, 200);
     }, function() {
         $(this).animate({color: '#ffffff'}, 200);
     })
     // END OF NAVIGATION BAR
+
+    // SUMMARY (SLIDESHOW)
+
+    
+
+    // END OF SUMMARY (SLIDESHOW)
 });
