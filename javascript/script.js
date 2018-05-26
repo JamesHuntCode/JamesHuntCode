ae9b277b1db4e416e55375cfc8d4adc4f30ec3fe
James Hunt <jameshuntwork1997@yahoo.com>
2018-05-26 01:10:23 +0100

    small updates - getting there
    new sections added - moved things around & corrected grammar

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-    
+    // Set Copyright Date
+    var copyrightNotice = $('#copyright');
+    var year = new Date().getFullYear();
+    copyrightNotice.html("© " + year + " James Hunt All Rights Reserved");
 });
