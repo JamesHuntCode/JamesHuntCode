584cfbba04ad0a505cac502158a3d56d214871ba
James Hunt <jameshuntwork1997@yahoo.com>
2018-05-26 23:41:20 +0100

    adding scroll to methods (soon) fixed footer elems

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,54 @@
 $(document).ready(function () {
+    // Scroll to page elements from the footer
+    $('#footer a').on('click', function () {
+
+        var linkClicked = $(this).text().toUpperCase();
+
+        var scrollToElem = function (where) {
+
+            // Handle scrolling here...
+
+        }
+
+        switch (linkClicked) {
+            case "TOP OF PAGE":
+
+            scrollToElem("navbar");
+
+            break;
+            case "ABOUT ME":
+
+            scrollToElem("about-me");
+
+            break;
+            case "MY SKILL SET":
+
+            scrollToElem("my-skills");
+
+            break;
+            case "PROJECT HIGHLIGHTS":
+
+            scrollToElem("projects");
+
+            break;
+            case "WORK & INTERESTS":
+
+            scrollToElem("current-interests");
+
+            break;
+            case "HIRE ME":
+
+            scrollToElem("hire-me");
+
+            break;
+            case "CONTACT ME":
+
+            scrollToElem("contact-me");
+
+            break;
+        }
+    });
+
     // Set Copyright Date
     var copyrightNotice = $('#copyright');
     var year = new Date().getFullYear();
